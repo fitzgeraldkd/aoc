3ab7ad213975604d9e6917386fc836913bcc7ea4
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-12-14 22:13:55 -0500

    Refactor day 2

--- 02-dive/02-dive.js
+++ 02-dive/02-dive.js
@@ -1,33 +1,36 @@
-const fs = require('fs');
-const inputs = fs
-  .readFileSync("inputs.txt", "utf8")
-  .split("\n")
-  // .map(item => parseInt(item, 10));
-  .map(item => item.trim());
+import fs from 'fs';
 
-const sample = fs
-  .readFileSync("sample.txt", "utf8")
-  .split("\n")
-  // .map(item => parseInt(item, 10));
-  .map(item => item.trim());
+const processInputs = (path="inputs.txt") => {
+  return fs
+    .readFileSync(path, "utf8")
+    .split("\n")
+    .map(instruction => 
+      instruction
+        .split(' ')
+        .map((value, index) => index === 1 ? parseInt(value) : value));
+};
 
-console.log(sample);
-console.log(inputs);
+const part1 = (path) => {
+  const inputs = processInputs(path);
+  const position = inputs.reduce((prev, curr) => {
+    prev[curr[0] === 'forward' ? 'hor' : 'ver'] += curr[1] * (curr[0] === 'up' ? -1 : 1)
+    return prev
+  }, {hor: 0, ver: 0})
+  return position.hor * position.ver;
+};
 
-let hor = 0;
-let ver = 0;
-let aim = 0;
-inputs.forEach(move => {
-  // console
-  let [direction, amount] = move.split(' ');
-  console.log(amount)
-  amount = parseInt(amount, 10);
-  if (direction === 'forward') {
-    hor += amount
-    ver += aim * amount
-  }
-  if (direction === 'down') aim += amount
-  if (direction === 'up') aim -= amount
-});
+const part2 = (path) => {
+  const inputs = processInputs(path);
+  const position = inputs.reduce((prev, curr) => {
+    if (curr[0] === 'forward') {
+      prev.hor += curr[1]
+      prev.ver += prev.aim * curr[1]
+    } else {
+      prev.aim += curr[1] * (curr[0] === 'up' ? -1 : 1)
+    }
+    return prev
+  }, {hor: 0, ver: 0, aim: 0})
+  return position.hor * position.ver;
+};
 
-console.log(hor * ver)
+export { part1, part2 };
