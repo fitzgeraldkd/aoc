cf2b9fc95cb8f267ec83d1f2d5020825edb70ae2
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-12-14 22:14:05 -0500

    Refactor day 3

--- 03-binary-diagnostic/03-binary-diagnostic.js
+++ 03-binary-diagnostic/03-binary-diagnostic.js
@@ -1,34 +1,70 @@
-const fs = require('fs');
-const inputs = fs
-  .readFileSync("inputs.txt", "utf8")
-  .split("\n")
-  // .map(item => parseInt(item, 10));
-  .map(item => item.trim());
+import fs from 'fs';
 
-const digits = [];
-
-console.log(inputs);
-inputs.forEach(item => {
-  for (let i=0; i<item.length; i++) {
-    if (digits[i] !== undefined) {
-      if (item[i] === '1') digits[i]++
-      else digits[i]--
-    } else digits[i] = 1;
-  }
-})
+const processInputs = (path="inputs.txt") => {
+  return fs
+    .readFileSync(path, "utf8")
+    .split("\n")
+    // .map(value => parseInt(value));
+};
 
 const countDigits = (inputs) => {
-  const digits = [];
-  inputs.forEach(item => {
+  return inputs.reduce((digits, item) => {
     for (let i=0; i<item.length; i++) {
       if (digits[i] !== undefined) {
         if (item[i] === '1') digits[i]++
         else digits[i]--
       } else digits[i] = item[i] === '1' ? 1 : -1;
     }
-  })
-  return digits
-}
+    return digits;
+  }, []);
+};
+
+const part1 = (path) => {
+  const inputs = processInputs(path);
+  const digitCount = countDigits(inputs);
+  const gamma = parseInt(digitCount.map(count => count > 0 ? 1 : 0).join(''), 2);
+  const epsilon = parseInt(digitCount.map(count => count < 0 ? 1 : 0).join(''), 2);
+  return gamma * epsilon;
+};
+
+const part2 = (path) => {
+  const inputs = processInputs(path);
+  
+};
+
+export { part1, part2 };
+
+// const fs = require('fs');
+// const inputs = fs
+//   .readFileSync("inputs.txt", "utf8")
+//   .split("\n")
+//   // .map(item => parseInt(item, 10));
+//   .map(item => item.trim());
+
+// const digits = [];
+
+// console.log(inputs);
+// inputs.forEach(item => {
+//   for (let i=0; i<item.length; i++) {
+//     if (digits[i] !== undefined) {
+//       if (item[i] === '1') digits[i]++
+//       else digits[i]--
+//     } else digits[i] = 1;
+//   }
+// })
+
+// const countDigits = (inputs) => {
+//   const digits = [];
+//   inputs.forEach(item => {
+//     for (let i=0; i<item.length; i++) {
+//       if (digits[i] !== undefined) {
+//         if (item[i] === '1') digits[i]++
+//         else digits[i]--
+//       } else digits[i] = item[i] === '1' ? 1 : -1;
+//     }
+//   })
+//   return digits
+// }
 
 // console.log(digits);
 // console.log(digits[1000])
@@ -45,7 +81,7 @@ const oxy = (values) => {
   }
 }
 
-console.log(oxy(inputs));
+// console.log(oxy(inputs));
 
 const co = (values) => {
   for (let i=0; i<values[0].length; i++) {
@@ -59,4 +95,4 @@ const co = (values) => {
   }
 }
 
-console.log(co(inputs));
+// console.log(co(inputs));
