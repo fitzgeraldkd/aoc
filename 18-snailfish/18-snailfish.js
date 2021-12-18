2ffb426b41880d0b898ef39480363421f3368586
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-12-18 02:12:39 -0500

    Refactor day 18

--- 18-snailfish/18-snailfish.js
+++ 18-snailfish/18-snailfish.js
@@ -10,15 +10,12 @@ const processInputs = (path="inputs.txt") => {
 };
 
 const checkExplode = (snailfish, depth=0, path=[]) => {
-  // console.log(snailfish, path)
   if (Array.isArray(snailfish)) {
-    if (depth === 4) {
-      // console.log('TO EXPLODE:', snailfish);
-      // explode('test')
-      return {snailfish, path};
-    }
+    if (depth === 4) return {snailfish, path};
+
     let exploded = checkExplode(snailfish[0], depth + 1, [...path, 0]);
     if (exploded) return exploded;
+    
     exploded = checkExplode(snailfish[1], depth + 1, [...path, 1]);
     if (exploded) return exploded;
   }
@@ -34,7 +31,6 @@ const explode = (snailfish, path, explodePair) => {
     const subPath = [...path.slice(0, path.lastIndexOf(1) + 1)];
     subPath[subPath.length-1] = 0;
 
-
     let affectedIndex = subPath.pop();
     let affectedSnailfish = snailfish;
     for (let i=0; i<subPath.length; i++) {
@@ -46,30 +42,11 @@ const explode = (snailfish, path, explodePair) => {
       affectedIndex = 1;
     }
     affectedSnailfish[affectedIndex] += explodePair[0];
-    // const subPath = [...path.slice(0, path.lastIndexOf(1) + 1)];
-    // subPath[subPath.length-1] = 0;
-    // let affectedSnailfish = snailfish;
-    // while (subPath.length > 0) {
-    //   const prevSnailfish = affectedSnailfish[subPath.shift()];
-    //   if (!Array.isArray(prevSnailfish)) break;
-    //   affectedSnailfish = prevSnailfish;
-    // }
-    // let affectedIndex = 1;
-    // while (Array.isArray(affectedSnailfish) && Array.isArray(affectedSnailfish[1])) {
-    //   if (affectedSnailfish[1] === removedSnailfish[path.at(-1)]) {
-    //     affectedIndex = 0;
-    //     break;
-    //   }
-    //   affectedSnailfish = affectedSnailfish[1];
-    // }
-    // affectedSnailfish[affectedIndex] += explodePair[0];
-    // console.log(affectedSnailfish);
   }
 
   if (!path.every(index => index === 1)) {
     const subPath = [...path.slice(0, path.lastIndexOf(0) + 1)];
     subPath[subPath.length-1] = 1;
-
 
     let affectedIndex = subPath.pop();
     let affectedSnailfish = snailfish;
@@ -81,39 +58,10 @@ const explode = (snailfish, path, explodePair) => {
       affectedSnailfish = affectedSnailfish[affectedIndex];
       affectedIndex = 0;
     }
-
-
-    // let affectedSnailfish = snailfish;
-    // while (subPath.length > 0) {
-    //   const nextSnailfish = affectedSnailfish[subPath.shift()];
-    //   if (!Array.isArray(nextSnailfish)) break;
-    //   affectedSnailfish = nextSnailfish;
-    // }
-    // console.log(affectedSnailfish)
-    // let affectedIndex = 0;
-    // while (Array.isArray(affectedSnailfish) && Array.isArray(affectedSnailfish[0])) {
-    //   if (affectedSnailfish[0] === removedSnailfish[path.at(-1)]) {
-    //     affectedIndex = 1;
-    //     break;
-    //   }
-    //   affectedSnailfish = affectedSnailfish[0];
-    // }
-    // console.log(affectedSnailfish)
     affectedSnailfish[affectedIndex] += explodePair[1];
   }
-  
-  
-  
-  // let removedSnailfish = snailfish;
-  // let lastIndex = path.pop();
-  // // console.log(path)
-  // while (path.length > 0) {
-  //   removedSnailfish = removedSnailfish[path.shift()];
-  // }
-  removedSnailfish[path.at(-1)] = 0;
-  // console.log(removedSnailfish);
 
-  // console.log('FINAL', JSON.stringify(snailfish))
+  removedSnailfish[path.at(-1)] = 0;
 }
 
 const checkSplit = (snailfish, path=[]) => {
@@ -133,29 +81,26 @@ const split = (snailfish, {value, path}) => {
   let affectedIndex = path.pop();
   let affectedSnailfish = snailfish;
   for (const index of path) {
-    // console.log(affectedSnailfish)
     affectedSnailfish = affectedSnailfish[index];
   }
   affectedSnailfish[affectedIndex] = [left, right];
-  // console.log(snailfish);
 };
 
 const reduce = snailfish => {
 
   while (true) {
     const toExplode = checkExplode(snailfish);
-    // console.log(toExplode);
     if (toExplode) {
-      // console.log('in the if')
       explode(snailfish, toExplode.path, toExplode.snailfish);
       continue;
     }
+
     const toSplit = checkSplit(snailfish);
     if (toSplit) {
       split(snailfish, toSplit);
-      // console.log('test');
       continue;
     }
+
     break;
   }
   return snailfish;
@@ -178,36 +123,17 @@ const getMagnitude = snailfish => {
 
 const part1 = (path) => {
   const inputs = processInputs(path);
-  // console.log(inputs);
-  // let snailfish = [inputs.shift(), inputs.shift()]
-  // console.log(snailfish)
-  // snailfish = reduce(snailfish)
   let results = [inputs.shift()];
   while (inputs.length > 0) {
     results = [results, inputs.shift()];
     results = reduce(results);
   }
-  // return JSON.stringify(results);
   return getMagnitude(results);
-
 };
 
 const part2 = (path) => {
   const inputs = processInputs(path);
-  // console.log(inputs);
-  for (const input of inputs) {
-    // console.log(JSON.stringify(input));
-  }
-  const magnitudes = inputs.map(input => getMagnitude(input));
-  for (const magnitude of magnitudes) {
-    // console.log(JSON.stringify(magnitude))
-  }
-
   let maxMagnitude = 0;
-  let inputA;
-  let inputB;
-  let reduced;
-  // let indeces;
   for (let i=0; i<inputs.length; i++) {
     for (let j=0; j<inputs.length; j++) {
       if (i !== j) {
@@ -215,34 +141,15 @@ const part2 = (path) => {
         const thisInputB = JSON.parse(JSON.stringify(inputs[j]))
         const thisReduced = reduce([thisInputA, thisInputB])
         const thisMagnitude = getMagnitude(thisReduced);
-        // maxMagnitude = Math.max(maxMagnitude, thisMagnitude);
-        if (thisMagnitude > maxMagnitude) {
-          inputA = inputs[i];
-          inputB = inputs[j];
-          reduced = thisReduced;
-          maxMagnitude = thisMagnitude;
-        }
+        maxMagnitude = Math.max(maxMagnitude, thisMagnitude);
       }
     }
   }
-  // console.log(JSON.stringify(inputA));
-  // console.log(JSON.stringify(inputB));
-  // console.log(JSON.stringify(reduced));
   return maxMagnitude
 
 };
 
-// console.log(part1());
+console.log(part1());
 console.log(part2());
-
-// check explode
-// console.log('Result:', JSON.stringify(reduce(  [[[[[9,8],1],2],3],4]  )));
-// console.log('Result:', JSON.stringify(reduce(  [7,[6,[5,[4,[3,2]]]]]  )));
-// console.log('Result:', JSON.stringify(reduce(  [[6,[5,[4,[3,2]]]],1]  )));
-// console.log('Result:', JSON.stringify(reduce(  [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]  )));
-// console.log('Result:', JSON.stringify(reduce(  [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]  )));
-
-// check split
-// console.log('Result:', JSON.stringify(reduce([[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]])));
 
 export { part1, part2 };
