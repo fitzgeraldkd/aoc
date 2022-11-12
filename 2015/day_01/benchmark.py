26767795739af6988f3fa0fe3ef03682aaf6742d
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2022-11-12 09:40:02 -0500

    Set benchmark repetitions as a constant

--- 2015/day_01/benchmark.py
+++ 2015/day_01/benchmark.py
@@ -1,12 +1,16 @@
 import timeit
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, os.path.pardir))
+
 
 import solution as day_01
+from utils.constants import BENCHMARK_REPTITIONS
 
-REPETITIONS = 1000
 
 def benchmark_day_1():
-    print(timeit.timeit(day_01.part_1, number=REPETITIONS))
-    print(timeit.timeit(day_01.part_2, number=REPETITIONS))
+    print(timeit.timeit(day_01.part_1, number=BENCHMARK_REPTITIONS))
+    print(timeit.timeit(day_01.part_2, number=BENCHMARK_REPTITIONS))
 
 
 if __name__ == '__main__':
