ecdd614f2d2fe3174c418cfcffdb4e5fe097a4b9
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2022-12-03 02:05:40 -0500

    Complete 2018 day 04

---
+++ 2018/day_04/solution.py
@@ -0,0 +1,68 @@
+import os
+import re
+import sys
+from collections import defaultdict
+from datetime import datetime
+from typing import Callable
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, os.path.pardir))
+
+from utils.setup import read_inputs
+
+
+def parse_input(input: str):
+    stripped_record = input.strip()
+    return [datetime.fromisoformat(stripped_record[1:17]), stripped_record[19:]]
+
+
+def get_inputs(parser: Callable):
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    return [parser(line) for line in read_inputs(script_directory)]
+
+
+def get_sleep_counts(records):
+    active_guard = None
+    sleep_counts = defaultdict(lambda: defaultdict(int))
+    sleep_minute = None
+
+    for record in records:
+        if '#' in record[1]:
+            active_guard = int(re.search(r'\#[0-9]+', record[1]).group(0)[1:])
+            sleep_minute = None
+        elif record[1] == 'falls asleep':
+            sleep_minute = record[0].minute
+        else:
+            for minute in range(sleep_minute, record[0].minute):
+                sleep_counts[active_guard][minute] += 1
+            sleep_minute = None
+
+    return sleep_counts
+
+
+def part_1(override_inputs = None):
+    records = get_inputs(parse_input) if override_inputs is None else override_inputs
+    sorted_records = sorted(records, key=lambda record: record[0])
+
+    sleep_counts = get_sleep_counts(sorted_records)
+
+    sleepiest_guard = max(sleep_counts.items(), key=lambda sleep_count: sum(sleep_count[1].values()))
+    sleepiest_minute = max(sleepiest_guard[1].items(), key=lambda minutes: minutes[1])[0]
+
+    return sleepiest_guard[0] *  sleepiest_minute
+
+
+def part_2(override_inputs = None):
+    records = get_inputs(parse_input) if override_inputs is None else override_inputs
+    sorted_records = sorted(records, key=lambda record: record[0])
+
+    sleep_counts = get_sleep_counts(sorted_records)
+
+    sleepiest_guard = max(sleep_counts.items(), key=lambda sleep_count: max(sleep_count[1].values()))
+    sleepiest_minute = max(sleepiest_guard[1].items(), key=lambda minutes: minutes[1])[0]
+
+    return sleepiest_guard[0] *  sleepiest_minute
+
+
+if __name__ == '__main__':
+    print('Part 1:', part_1())
+    print('Part 2:', part_2())
--- 2018/tests.py
+++ 2018/tests.py
@@ -3,8 +3,9 @@ import unittest
 import day_01.tests as day_01
 import day_02.tests as day_02
 import day_03.tests as day_03
+import day_04.tests as day_04
 
-TEST_MODULES = [day_01, day_02, day_03]
+TEST_MODULES = [day_01, day_02, day_03, day_04]
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
