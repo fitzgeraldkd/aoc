668fe87b642bba6ed6027f2c55e10458ba16978f
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2022-12-06 00:33:20 -0500

    Complete 2022 day 6

---
+++ 2022/day_06/solution.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from collections import Counter
+from typing import Callable
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, os.path.pardir))
+
+from utils.setup import read_inputs
+
+
+def parse_input(input: str):
+    return input.strip()
+
+
+def get_inputs(parser: Callable):
+    script_directory = os.path.dirname(os.path.realpath(__file__))
+    return [parser(line) for line in read_inputs(script_directory)][0]
+
+
+def is_marker(characters: str):
+    return len(characters) == len(Counter(characters))
+
+
+def part_1(override_inputs = None):
+    datastream = get_inputs(parse_input) if override_inputs is None else override_inputs
+
+    marker_length = 4
+    for index in range(marker_length, len(datastream)):
+        if is_marker(datastream[index - marker_length:index]):
+            return index
+
+
+def part_2(override_inputs = None):
+    datastream = get_inputs(parse_input) if override_inputs is None else override_inputs
+
+    marker_length = 14
+    for index in range(marker_length, len(datastream)):
+        if is_marker(datastream[index - marker_length:index]):
+            return index
+
+
+if __name__ == '__main__':
+    print('Part 1:', part_1())
+    print('Part 2:', part_2())
