3bd2ef8ad9c1021592f329e1e84006be95857bd4
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2022-12-12 22:15:52 -0500

    Improve 2022 day 12

--- 2022/day_12/solution.py
+++ 2022/day_12/solution.py
@@ -5,6 +5,7 @@ from typing import Callable
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, os.path.pardir))
 
+from classes.PriorityQueue import PriorityQueue
 from utils.pathing import a_star, get_adjacent, get_manhattan_distance
 from utils.setup import read_inputs
 from utils.constants import ALPHABET
@@ -23,12 +24,6 @@ def is_in_boundaries(coords, grid):
     return min(coords) >= 0 and coords[0] < len(grid[0]) and coords[1] < len(grid)
 
 
-def get_is_wall(neighbor, current, grid):
-    if not is_in_boundaries(neighbor, grid):
-        return True
-    return ALPHABET.index(grid[neighbor[1]][neighbor[0]]) > ALPHABET.index(grid[current[1]][current[0]]) + 1
-
-
 def part_1(override_inputs = None):
     grid = get_inputs(parse_input) if override_inputs is None else override_inputs
 
@@ -44,7 +39,10 @@ def part_1(override_inputs = None):
                 end = (x, y)
                 grid[y][x] = 'z'
 
-    is_wall = lambda neighbor, current: get_is_wall(neighbor, current, grid)
+    def is_wall(neighbor, current):
+        if not is_in_boundaries(neighbor, grid):
+            return True
+        return ALPHABET.index(grid[neighbor[1]][neighbor[0]]) > ALPHABET.index(grid[current[1]][current[0]]) + 1
 
     return len(a_star(start, end, is_wall, get_heuristic=get_manhattan_distance)) - 1
 
@@ -53,41 +51,32 @@ def part_2(override_inputs = None):
     grid = get_inputs(parse_input) if override_inputs is None else override_inputs
 
     end = None
-    possible_starts = set()
 
     for y, row in enumerate(grid):
         for x, cell in enumerate(row):
             if cell == 'S':
-                possible_starts.add((x, y))
                 grid[y][x] = 'a'
             elif cell == 'E':
                 end = (x, y)
                 grid[y][x] = 'z'
-            elif cell == 'a':
-                possible_starts.add((x, y))
 
-    is_wall = lambda neighbor, current: get_is_wall(neighbor, current, grid)
+    def is_wall(neighbor, current):
+        if not is_in_boundaries(neighbor, grid):
+            return True
+        return ALPHABET.index(grid[neighbor[1]][neighbor[0]]) + 1 < ALPHABET.index(grid[current[1]][current[0]])
 
-    possible_paths = {}
-
-    def discard_adjacent(coords):
-        for adjacent in get_adjacent(coords):
-            if is_in_boundaries(adjacent, grid) and adjacent in possible_starts and adjacent not in possible_paths:
-                possible_paths[adjacent] = math.inf
-                discard_adjacent(adjacent)
-
-    for start in possible_starts:
-        if start not in possible_paths:
-            path = a_star(start, end, is_wall, get_heuristic=get_manhattan_distance)
-            if path is not None:
-                possible_paths[start] = len(path)
-                for index, node in [(index, node) for index, node in enumerate(path) if node in possible_starts]:
-                    possible_paths[node] = index + 1
-            else:
-                possible_paths[start] = math.inf
-                discard_adjacent(start)
-
-    return min(possible_paths.values()) - 1
+    queue = [(neighbor, 1) for neighbor in get_adjacent(end) if not is_wall(neighbor, end)]
+    checked = { end }
+    while queue:
+        node, distance = queue.pop(0)
+        checked.add(node)
+        if grid[node[1]][node[0]] == 'a':
+            return distance
+        else:
+            for neighbor in get_adjacent(node):
+                if not is_wall(neighbor, node) and neighbor not in checked:
+                    queue.append((neighbor, distance + 1))
+                    checked.add(neighbor)
 
 
 if __name__ == '__main__':
