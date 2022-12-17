08f4f0d0eba89837c8ad4d3764e01078446614ac
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2022-12-17 16:30:02 -0500

    Add util for tqdm kwargs

--- utils/setup.py
+++ utils/setup.py
@@ -1,3 +1,14 @@
+import os
+
+
+def get_tqdm_kwargs(file: str, part: int, disable=False):
+    return {
+        'desc': f'{os.path.basename(os.path.dirname(file))} part {part}',
+        'disable': disable,
+        'leave': False,
+    }
+
+
 def read_inputs(script_directory: str, filename='inputs.txt'):
 
     with open(f'{script_directory}/{filename}') as file:
