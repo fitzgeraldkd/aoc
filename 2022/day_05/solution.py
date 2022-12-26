da94eac930012158fa214f54ad09ee044a53a3ef
Kenneth Fitzgerald <kenneth.fitzgerald@heyartifact.com>
2022-12-25 23:33:14 -0500

    Parse initial state from inputs

--- 2022/day_05/solution.py
+++ 2022/day_05/solution.py
@@ -1,9 +1,11 @@
 import os
+import re
 import sys
 from typing import Callable
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, os.path.pardir))
 
+from utils.lists import split
 from utils.setup import read_inputs
 
 
@@ -13,26 +15,22 @@ def parse_input(input: str):
 
 
 def get_inputs(parser: Callable):
-    return [parser(line) for line in read_inputs(__file__)]
+    initial_state, instructions = split(read_inputs(__file__), '\n')
+    stacks = []
+    for match in re.finditer(r'\d', initial_state[-1]):
+        stack = []
+        for row in initial_state[-2::-1]:
+            if len(row) <= match.start() or row[match.start()] == ' ':
+                break
+            stack.append(row[match.start()])
+        stacks.append(stack)
+    return stacks, [parser(line) for line in instructions]
 
 
 def part_1(override_inputs = None):
-    inputs = get_inputs(parse_input) if override_inputs is None else override_inputs
+    stacks, instructions = get_inputs(parse_input) if override_inputs is None else override_inputs
 
-    # TODO: Parse initial stacks from the inputs instead of hard-coding.
-    stacks = [
-        ['N', 'R', 'G', 'P'],
-        ['J', 'T', 'B', 'L', 'F', 'G', 'D', 'C'],
-        ['M', 'S', 'V'],
-        ['L', 'S', 'R', 'C', 'Z', 'P'],
-        ['P', 'S', 'L', 'V', 'C', 'W', 'D', 'Q'],
-        ['C', 'T', 'N', 'W', 'D', 'M', 'S'],
-        ['H', 'D', 'G', 'W', 'P'],
-        ['Z', 'L', 'P', 'H', 'S', 'C', 'M', 'V'],
-        ['R', 'P', 'F', 'L', 'W', 'G', 'Z']
-    ]
-
-    for quantity, source, destination in inputs:
+    for quantity, source, destination in instructions:
         for _ in range(quantity):
             stacks[destination - 1].append(stacks[source - 1].pop())
 
@@ -40,22 +38,9 @@ def part_1(override_inputs = None):
 
 
 def part_2(override_inputs = None):
-    inputs = get_inputs(parse_input) if override_inputs is None else override_inputs
+    stacks, instructions = get_inputs(parse_input) if override_inputs is None else override_inputs
 
-    # TODO: Parse initial stacks from the inputs instead of hard-coding.
-    stacks = [
-        ['N', 'R', 'G', 'P'],
-        ['J', 'T', 'B', 'L', 'F', 'G', 'D', 'C'],
-        ['M', 'S', 'V'],
-        ['L', 'S', 'R', 'C', 'Z', 'P'],
-        ['P', 'S', 'L', 'V', 'C', 'W', 'D', 'Q'],
-        ['C', 'T', 'N', 'W', 'D', 'M', 'S'],
-        ['H', 'D', 'G', 'W', 'P'],
-        ['Z', 'L', 'P', 'H', 'S', 'C', 'M', 'V'],
-        ['R', 'P', 'F', 'L', 'W', 'G', 'Z']
-    ]
-
-    for quantity, source, destination in inputs:
+    for quantity, source, destination in instructions:
         stacks[destination - 1].extend(stacks[source - 1][-1 * quantity:])
         stacks[source - 1] = stacks[source - 1][:-1 * quantity]
 
--- 2022/day_05/tests.py
+++ 2022/day_05/tests.py
@@ -1,9 +1,11 @@
-import unittest
 import os
 import sys
+import unittest
+from unittest.mock import patch
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir))
 
 from day_05.solution import part_1, part_2
+from utils.setup import read_inputs
 
 
 class TestBase(unittest.TestCase):
@@ -16,7 +18,14 @@ class TestBase(unittest.TestCase):
 
 
 class TestExamples(unittest.TestCase):
-    pass
+
+    def test_part_1(self):
+        with patch('day_05.solution.read_inputs', return_value=read_inputs(__file__, 'sample.txt')):
+            self.assertEqual(part_1(), 'CMZ')
+
+    def test_part_2(self):
+        with patch('day_05.solution.read_inputs', return_value=read_inputs(__file__, 'sample.txt')):
+            self.assertEqual(part_2(), 'MCD')
 
 
 class TestUtils(unittest.TestCase):
