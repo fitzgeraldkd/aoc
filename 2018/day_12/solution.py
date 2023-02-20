748914f46fcdd6698a6682eb72b018d27b51d21d
Kenny <kfonatoko@gmail.com>
2023-02-19 21:15:44 -0500

    Complete 2018 day 12

--- 2018/day_12/solution.py
+++ 2018/day_12/solution.py
@@ -8,28 +8,52 @@ from utils.lists import split
 from utils.setup import read_inputs
 
 
-def parse_input(input: str):
-    return input.strip()
-
-
-def get_inputs(parser: Callable):
+def get_inputs():
     initial_state, notes = split(read_inputs(__file__), '\n')
-    notes = [note.split(' => ') for note in notes]
-    plants = { item for index, item in enumerate(initial_state.split(': ')[1]) if item == '#' }
+    notes = [note.strip().split(' => ') for note in notes]
+    plants = { index for index, item in enumerate(initial_state[0].strip().split(': ')[1]) if item == '#' }
     notes = { pattern: output for pattern, output in notes }
-    return [parser(line) for line in read_inputs(__file__)]
+    return plants, notes
+
+
+def get_key(plants, index: int):
+    return ''.join([('#' if i in plants else '.') for i in range(index - 2, index + 3)])
+
+
+def run_generations(plants: set, notes: dict, generations: int):
+    min_index = min(plants) - 2
+    max_index = max(plants) + 2
+    differences = [None for _ in range(5)]
+
+    for generation in range(generations):
+        new_plants = set()
+        for index in range(min_index, max_index + 1):
+            if notes.get(get_key(plants, index), '.') == '#':
+                new_plants.add(index)
+                min_index = min(min_index, index - 2)
+                max_index = max(max_index, index + 2)
+
+        differences.pop(0)
+        differences.append(sum(new_plants) - sum(plants))
+
+        # Eventually the sum of the plants will continue growing at a constant rate.
+        if all([difference is not None for difference in differences]) \
+            and len(set(differences)) == 1:
+            return sum(plants) + (generations - generation) * differences[0]
+
+        plants = new_plants
+
+    return sum(plants)
 
 
 def part_1(override_inputs = None):
-    inputs = get_inputs(parse_input) if override_inputs is None else override_inputs
-
-    return None
+    plants, notes = get_inputs() if override_inputs is None else override_inputs
+    return run_generations(plants, notes, generations=20)
 
 
 def part_2(override_inputs = None):
-    inputs = get_inputs(parse_input) if override_inputs is None else override_inputs
-
-    return None
+    plants, notes = get_inputs() if override_inputs is None else override_inputs
+    return run_generations(plants, notes, generations=50000000000)
 
 
 if __name__ == '__main__':
--- 2018/day_12/tests.py
+++ 2018/day_12/tests.py
@@ -1,24 +1,29 @@
 import unittest
 import os
 import sys
+from unittest.mock import patch
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir))
 
-from day_XX.solution import part_1, part_2
+from day_12.solution import part_1, part_2
+from utils.setup import read_inputs
+
+CURRENT_DIRECTORY = os.path.realpath(__file__)
 
 
 class TestBase(unittest.TestCase):
 
-    @unittest.expectedFailure
     def test_part_1(self):
-        self.assertEqual(part_1(), None)
+        self.assertEqual(part_1(), 2736)
 
-    @unittest.expectedFailure
     def test_part_2(self):
-        self.assertEqual(part_2(), None)
+        self.assertEqual(part_2(), 3150000000905)
 
 
 class TestExamples(unittest.TestCase):
-    pass
+
+    @patch('day_12.solution.read_inputs', return_value=read_inputs(CURRENT_DIRECTORY, 'sample.txt'))
+    def test_part_1(self, mocked_read_inputs):
+        self.assertEqual(part_1(), 325)
 
 
 class TestUtils(unittest.TestCase):
